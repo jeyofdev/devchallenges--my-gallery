70164e96eaceeb9e2f0551df70850a277ac6a1c5
jeyofdev <jgregoire.pro@gmail.com>
2020-12-22 09:45:43 +0100

    feat : create the animation of a counter

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,11 @@
+import Counter from './modules/Counter';
 import '../styles/app.scss';
+
+const postsNumber = document.getElementById('posts-number');
+Counter(postsNumber, 100, 3000);
+
+const followersNumber = document.getElementById('followers-number');
+Counter(followersNumber, 2242, 6000);
+
+const followingNumber = document.getElementById('following-number');
+Counter(followingNumber, 1432, 4500);
